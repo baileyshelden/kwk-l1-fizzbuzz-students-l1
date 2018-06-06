0125e5a3e658d61002a1285612de334c7f2edbb8
baileyshelden <bailey7748@gmail.com>
2018-06-06 20:10:01 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,40 @@
+# if a  # is diviible by 3 it returns fizz
+#  if a / by 5 it returns buzz
+#  id a /by 3 and 5 it returns fizzbuzz 
+#  if a / by 4 it returns nill
+
+# def division(num)
+#   # num = 45
+#   if num % 3 == 0 && num % 5 == 0 
+#   puts "fizzbuzz"
+#   elsif num % 3 == 0 
+#     puts "fizz"
+#   elsif num % 5 == 0
+#   puts "buzz"
+#   else num % 4 == 0 
+#     puts "nil"
+
+
+# end  
+# end
+
+
+# division(45)
+
+def division(num)
+  # num = 45
+  if num % 3 == 0 && num % 5 == 0 
+  puts "fizzbuzz"
+  elsif num % 3 == 0 
+    puts "fizz"
+  elsif num % 5 == 0
+   puts "buzz"
+  else num % 4 == 0 
+    puts "nil"
+
+
+end  
+end
+
+
+division(45�)
